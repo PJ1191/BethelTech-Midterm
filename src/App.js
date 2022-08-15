ecf4807d2c0e756d4892779fefee2153c9d70718
PJ1191 <PJ1191@gmail.com>
2022-08-15 08:25:03 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GuessingGame from './GuessingGame';
 
 function App() {
+  const [guess, setGuess] = useState([])
+
+
+  const handleGuess = (number) => {
+
+    setGuess(guess.concat(number))
+
+  }
+
+  function log(){
+    return(
+      <>
+      </>
+    )
+  }
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <GuessingGame />
+      <GuessingGame onGuess={handleGuess} onReset={log}/>
     </div>
   );
 }
--- src/GuessingGame.js
+++ src/GuessingGame.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-function GuessingGame() {
+function GuessingGame(props) {
+    const [number, setNumber] = useState([])
+    const [count, setCount] = useState(0)
+
+    const handlerValue = (e) => {
+        setCount(e.target.value);
+    };
+
+    function handleGuess(event) {
+        setNumber({ ...number, [event.target.name]: event.target.value });
+    }
+
+    function handleSubmit() {
+        setCount(count+1)
+        console.log(`You Dun Guessed`, count+1, `Times.`)
+       
+    }
+
+    function handleReset() {
+        props.onReset(count)
+        setCount(null)
+        console.log(`You Dun Reset `, count)
+
+    }
+
+    var theNumber = Math.floor((Math.random() * 100) + 1);
+    var userSelect = 0;
+
+
     return (
         <div>
             <div>
                 <p>I am thinking of a number between 1 and 100. Guess the Lucky Number!</p>
-                <p>You have made {'{blank}'} guesses</p>
+                <p>You have made {count} guesses</p>
             </div>
             <div>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" >
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" name="name" />
+                        <Form.Control type="text" name="name" onChange={handleGuess} />
                     </Form.Group>
                 </Form>
-                <Button type='GuessSend'>Guess</Button>
-                <br/>
-                <Button type='Reset'>Reset</Button>
-
+                <Button type='GuessSend' onClick={handleSubmit}>Guess</Button>
+                <br />
+                <Button type='Reset' onClick={handleReset}>Reset</Button>
             </div>
+
         </div>
+
 
 
     )
